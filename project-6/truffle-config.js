84242cff467306ae52c8c0e033bade0d323cff5f
NicoleMichielin <michielinnicole@gmail.com>
2024-08-12 19:33:19 +0200

    Update truffle-config.js

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,4 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
+  //require('dotenv').config();
 
   contracts_build_directory: "./build/contracts",
   contracts_directory: "./contracts",
@@ -11,13 +14,19 @@ module.exports = {
       websockets: true
     },
     
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "4777",       // Any network (default: none)
+    },
+
     sepolia: {
       provider: () => new HDWalletProvider({
         mnemonic: process.env.MNEMONIC,
         providerOrUrl: `https://sepolia.infura.io/v3/f4c83644ae044a20bc2a56927e675663`,
         numberOfAddresses: 1,
         shareNonce: true,
-        pollingInterval: 15000 // aumento dell'intervallo di polling
+        //pollingInterval: 15000 // aumento dell'intervallo di polling
       }),
       network_id: 11155111,  // Sepolia's id
       //gas: 5500000,         // Gas limit
@@ -27,19 +36,20 @@ module.exports = {
       skipDryRun: true,      // Skip dry run before migrations
       deploymentPollingInterval: 15000,
       networkCheckTimeout: 1000000
-    }
-  },
-  compilers: {
-    solc: {
-       version: "0.8.20",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-       settings: {          // See the solidity docs for advice about optimization and evmVersion
-        optimizer: {
-          enabled: true,
-          runs: 200
-        },
-        evmVersion: "shanghai"
-     }
+    },
+
+    compilers: {
+      solc: {
+         version: "0.8.20",    // Fetch exact version from solc-bin (default: truffle's version)
+        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+         settings: {          // See the solidity docs for advice about optimization and evmVersion
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+          evmVersion: "shanghai"
+       }
+      }
     }
   }
-};
+}
